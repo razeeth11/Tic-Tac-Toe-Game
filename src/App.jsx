47f508f3cd8ca981343bba6a0af867cde898f05f
razeeth11 <razeeth011@gmail.com>
2023-04-17 12:08:03 +0530

    UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 
 export function App() {
 
+  const box = ["" , "", "", "", "", "", "", "", ""]
+
   return (
     <div className="App">
-      
+      <h1>Tic Tac Toe</h1>
+       <div className="boxes">
+         {box.map((b,key)=> <Box key={key}/>)}
+       </div>
+    </div>
+  )
+}
+
+export function Box(){
+
+  const [value,setValue] = useState()
+
+  const style = {
+    color : value ?  "rgb(84, 84, 84)" : '#FFFCCA' ,
+  }
+  return(
+    <div style={style} onClick={()=> setValue(!value)} className="box">
+        {value}
     </div>
   )
 }
